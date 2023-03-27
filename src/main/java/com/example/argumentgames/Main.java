3429b47cfabd6a1463a5e1cf1407fd1ee1c5f571
ramzes222 <sebastian.oleksa@gmail.com>
2023-03-27 13:20:54 +0100

    Add App icon

--- src/main/java/com/example/argumentgames/Main.java
+++ src/main/java/com/example/argumentgames/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,6 +17,8 @@ public class Main extends Application {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main-view.fxml")));
             Scene scene = new Scene(root);
             stage.setTitle("Argument Games v1.0");
+            Image appIcon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("img/BuildTree.png")));
+            stage.getIcons().add(appIcon);
 
             stage.setScene(scene);
             stage.show();
